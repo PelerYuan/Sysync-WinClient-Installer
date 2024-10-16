7793ea749e56c6cbb07926f09707347351951b0e
PelerYuan <pelerlittlepig@gmail.com>
2024-10-16 12:44:46 +0800

    auto start

--- tools/install_service.bat
+++ tools/install_service.bat
@@ -2,4 +2,3 @@ cd "%~dp0"
 .\nssm install SysyncWin "%~dp0\..\SysyncWinClient.exe"
 .\nssm start SysyncWin
 .\allow_firewall.bat
-pause
--- tools/remove_service.bat
+++ tools/remove_service.bat
@@ -1,4 +1,3 @@
 cd "%~dp0"
 .\nssm stop SysyncWin "%~dp0\..\SysyncWinClient.exe"
 .\nssm remove SysyncWin confirm
-pause
